b508c530393e511753504ac3fe3aa5ca810d85a6
Morpilou <sallaino@etml.educanet2.ch>
2018-05-31 15:42:41 +0200

    fix animalManager

--- project/src/php/model/animalManager.php
+++ project/src/php/model/animalManager.php
@@ -9,14 +9,13 @@
 class animalManager
 {
 
-    public $id;
 
     public function getAnimals()
     {
 
         $db = $this->dbConnect();
 
-        $req = $db->query('SELECT * FROM t_animal INNER JOIN t_race on t_animal.fkRace = t_race.idRace INNER JOIN t_type on t_race.fkIdType = t_type.idType');
+        $req = $db->query('SELECT * FROM t_animal INNER JOIN t_race on t_animal.fkRace = t_race.idRace INNER JOIN t_typeandrace on t_race.idRace = t_typeandrace.fkRace INNER JOIN t_type on t_typeandrace.fkidType = t_type.idType');
 
         return $req;
 
@@ -30,18 +29,90 @@ class animalManager
 
         if (isset($id) && !empty($id)) {
 
-            $req = $db->query('SELECT * FROM t_animal INNER JOIN t_race on t_animal.fkRace = t_race.idRace INNER JOIN t_type on t_race.fkIdType = t_type.idType WHERE t_animal.idAnimal =' . $id);
-
+            $req = $db->query('SELECT * FROM t_animal INNER JOIN t_race on t_animal.fkRace = t_race.idRace INNER JOIN t_typeandrace on t_race.idRace = t_typeandrace.fkRace INNER JOIN t_type on t_typeandrace.fkidType = t_type.idType  WHERE t_animal.idAnimal =' . $id);
 
             return $req;
 
-
         } else {
-            throw new Exception('ID incorrect');
+            throw new Exception('Error : pas d\'id');
         }
 
 
     }
+
+
+    public function getRace()
+    {
+
+        $db = $this->dbConnect();
+
+        $req = $db->query('SELECT * FROM  t_race  INNER JOIN t_typeandrace on t_race.idRace = t_typeandrace.fkRace INNER JOIN t_type on t_typeandrace.fkidType = t_type.idType WHERE t_typeandrace.fkidType = t_type.idType AND t_race.idRace = t_typeandrace.fkRace');
+
+        return $req;
+
+
+    }
+
+    public function getTypeAndRace()
+    {
+
+
+        $db = $this->dbConnect();
+
+
+        $req = $db->query('SELECT * FROM  t_typeandrace  INNER JOIN t_type on t_typeandrace.fkidType = t_type.idType INNER JOIN t_race on t_typeandrace.fkRace = t_race.idRace');
+        return $req;
+
+
+    }
+
+    public function addAnimal($aniName, $aniBirthDate, $aniChipNumber, $aniLinkPhoto, $fkIdUser, $fkIdRace)
+    {
+
+        if (!empty($aniName) && !empty($aniBirthDate) && !empty($fkIdUser) && ($fkIdRace)) {
+            $db = $this->dbConnect();
+
+            $add = $db->exec('INSERT INTO t_animal(aniName,aniBirthDate,aniChipNumber,aniLinkPhoto,fkIdUser,fkIdRace)VALUES(' . $aniName . ',' . $aniBirthDate . ',' . $aniChipNumber . ',' . $aniLinkPhoto . ',' . $fkIdUser . ',' . $fkIdRace . ')');
+
+        }
+    }
+
+    public function getAnimalWeight($id)
+    {
+
+
+        $db = $this->dbConnect();
+
+        if (isset($id) && !empty($id)) {
+
+            $wei = $db->query('SELECT * FROM t_weight  WHERE t_weight.fkIdAnimal =' . $id);
+
+            return $wei;
+
+        } else {
+            throw new Exception('pas d\'id');
+        }
+
+
+    }
+
+    public function deleteAnimal($id)
+    {
+
+        $db = $this->dbConnect();
+
+        if (isset($id) && !empty($id)) {
+
+            $del = $db->query('UPDATE t_animal SET t_animal.aniDeleted = 1 WHERE t_animal.idAnimal =' . $id);
+
+            return $del;
+
+        } else {
+            throw new Exception('Error : pas d\'id');
+        }
+
+    }
+
 
     private function dbConnect()
     {
@@ -49,7 +120,7 @@ class animalManager
             $db = new PDO('mysql:host=localhost;dbname=db_gestanimalhealth;charset=utf8', 'root', '');
             return $db;
         } catch (Exception $e) {
-            die('Erreur : ' . $e->getMessage());
+            die($e->getMessage());
         }
 
     }
