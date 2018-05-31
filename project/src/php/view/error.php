2058b6d3ac1ce13e96a9c777926a8ca4e4303adf
Morpilou <sallaino@etml.educanet2.ch>
2018-05-31 15:40:56 +0200

    fix error.php

--- project/src/php/view/error.php
+++ project/src/php/view/error.php
@@ -15,6 +15,18 @@ $nav = '';
 
 //$req->closeCursor();
 
+?>
+
+    <div class="alert alert-info" role="alert">
+        <h4 class="alert-heading">Oups !</h4>
+        <p><?= $error ?></p>
+        <hr>
+        <p class="mb-0">Vous serrez redirigé automatiquement dans 5 seconds !</p>
+    </div>
+
+<?php
+
+
 
 $content = ob_get_clean();
 
