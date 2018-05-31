3fd968ffcc7953521e0671ae1a64c4dbc12b86be
Morpilou <sallaino@etml.educanet2.ch>
2018-05-31 15:42:07 +0200

    fix detail animal

--- project/src/php/view/detailsAnimal.php
+++ project/src/php/view/detailsAnimal.php
@@ -8,7 +8,9 @@
 
 $title = 'Pet La Forme - Détails';
 
+
 ob_start();
+
 
 $nav = ' <li class="nav-item">
             <a class="btn btn-link color-link-login" style="display: inline-block;text-decoration: none;" href="" ><b>Détails</b></span></a>
@@ -20,29 +22,120 @@ $nav = ' <li class="nav-item">
            <a class="btn btn-link color-link-login" style="display: inline-block;text-decoration: none;" href="" >Rappels</span></a>
           </li>\';';
 
+
 $data = $req->fetch(PDO::FETCH_ASSOC);
 
-$birthDate = new DateTime($data['aniBirthDate'])
+
+if ($data == false) {
+  throw new Exception('Aucune données à afficher, veuillez vérifier l\'id ! ');
+}elseif ($data['aniDeleted'] == true){
+    throw new Exception('Cet animal à été supprimé !');
+}
+
+$birthDate = new DateTime($data['aniBirthDate']);
+
+
 ?>
 
-    <main role="main" class="container">
+<main role="main" class="container">
 
-    <div>
-        <img src="<?= $data['aniLinkPhoto'] ?>" class="img-fluid" style="width: 200px;height: 200px" alt="test">
+    <div class="row">
+        <div class="col-sm-5">
+            <div>
+                <img src="<?= $data['aniLinkPhoto'] ?>" class="img-fluid " alt="test">
 
+            </div>
+            <br>
+
+            <div class="row">
+                <div class="col-lg-5">
+                    <span id="labelType"><b>Type - Race : </b></span>
+                </div>
+                <div class="col-lg-5">
+                    <p><?= $data['typType'] . ' - ' . $data['racRace'] ?></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-5">
+                    <span><b>Nom : </b></span>
+                </div>
+                <div class="col-lg-5">
+                    <p><?= $data['aniName'] ?></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-5">
+                    <span><b>Date de naissance : </b></span>
+                </div>
+                <div class="col-lg-5">
+                    <p><?= $birthDate->format('d.m.Y') ?></p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-5">
+                    <span><b>Numéro de la puce : </b></span>
+                </div>
+                <div class="col-lg-5">
+                    <p><?= $data['aniChipNumber'] ?></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <form action="">
+                <div class="form-group">
+                    <label for="from">Du :</label>
+                    <input type="date" class="form-control" id="from">
+                <div class="form-group">
+                    <label for="to">Au :</label>
+                    <input type="date" class="form-control" id="to">
+                </div>
+                <button type="submit" class="btn btn-success" style="float:right ">Visualiser</button>
+            </form>
+            <br>
+            <br>
+            <div  id="weightChart" style="width: 100%; height: 250px;"></div>
+        </div>
     </div>
-    <br>
-    <div>
-        <p><?= $data['typType'] . ' - ' . $data['racRace'] ?></p>
-        <p><?= $data['aniName'] ?></p>
-        <p><?= $birthDate->format('d.m.Y') ?></p>
-        <p><?= $data['aniChipNumber'] ?></p>
-    </div>
-    </main><!-- /.container -->
+
+
+</main><!-- /.container -->
 <?php
 
-//$req->closeCursor();
+$req->closeCursor();
+$content = ob_get_contents();
+ob_end_clean();
 
-$content = ob_get_clean();
 
 require('template.php');
+?>
+
+<script>
+    new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'weightChart',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: [
+
+            <?php
+
+            while ($weiAni = $wei->fetch(PDO::FETCH_ASSOC)) {
+                $weiDate = new DateTime($weiAni['weiDate']);
+                echo "{ period:\"" . $weiDate->format('Y-m-d') . "\", value:" . $weiAni['weiWeight'] . "},";
+
+            }
+
+            ?>
+
+        ],
+        // The name of the data record attribute that contains x-values.
+        xkey: 'period',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['value'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Poids']
+    });
+</script>
