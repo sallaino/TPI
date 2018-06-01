f3b5a06e7da955c5870ffe39ed9ccec65518272c
Morpilou <sallaino@etml.educanet2.ch>
2018-06-01 15:42:39 +0200

    add login function

--- project/src/php/controller/controllerManager.php
+++ project/src/php/controller/controllerManager.php
@@ -1,26 +1,69 @@
-<?php
+    <?php
 
-/**
- * Created by PhpStorm.
- * User: sallaino
- * Date: 28.05.2018
- * Time: 10:59
- */
-class controllerManager
-{
-    public function getPath()
+    /**
+    * Created by PhpStorm.
+    * User: sallaino
+    * Date: 28.05.2018
+    * Time: 10:59
+    */
+    class controllerManager
     {
 
-       $path = $_SERVER['PHP_SELF'];
+      public function getPath()
+      {
+
+        require('./view/login.php');
+        $path = $_SERVER['PHP_SELF'];
 
 
-       return $path;
+        return $path;
+
+      }
+      public function login()
+      {
 
 
-    }
+        require('./model/animalManager.php');
+        $animalManager = new animalManager();
 
-    public function indexAction()
-    {
+
+
+          if(!empty($_POST['useIdentifier']) && !empty($_POST['usePassword'])){
+
+            $useName =  $_POST['useIdentifier'];
+            $usePassword =  $_POST['usePassword'];
+
+
+            $login = $animalManager->getLoginData($useName,$usePassword);
+
+
+            $getLogin = $login->fetch(PDO::FETCH_ASSOC);
+
+
+            $a = password_hash('test',PASSWORD_DEFAULT);
+            if (password_verify($usePassword,$a)) {
+
+
+              header('Location:./router.php?action=indexAction');
+
+            }else {
+
+
+              require('./view/login.php');
+            }
+
+
+
+
+
+          }else {
+          require('./view/login.php');
+          }
+
+      }
+
+      public function indexAction()
+      {
         require('./model/animalManager.php');
 
         $animalManager = new animalManager();
@@ -28,24 +71,52 @@ class controllerManager
 
         require('./view/index.php');
 
-    }
+      }
 
-    public function addAnimal()
-    {
+
+      public function addAnimal()
+      {
 
         require('./model/animalManager.php');
 
         $animalManager = new animalManager();
-        $req = $animalManager->getAnimals();
-        $race = $animalManager->getRace();
         $typeandrace = $animalManager->getTypeAndRace();
+
+
+        if (!empty($_POST)) {
+
+          $animalName = $_POST['aniName'];
+          $animalBirthDate = $_POST['aniBirthDate'];
+
+          if (empty($_POST['aniChipNumber'])) {
+            $_POST['aniChipNumber'] = NULL;
+
+            $aniChipNumber = $_POST['aniChipNumber'];
+
+          }else {
+            $aniChipNumber = $_POST['aniChipNumber'];
+          }
+
+          if (empty($_POST['aniLinkPhoto'])){
+            $_POST['aniLinkPhoto'] = './../../resources/images/animaldefault.jpg';
+
+            $aniLinkPhoto =  $_POST['aniLinkPhoto'];
+          }else {
+            $aniLinkPhoto = $_POST['aniLinkPhoto'];
+          }
+          $aniRace = $_POST['aniRace'];
+
+
+          $addAnimalInformations = $animalManager->addAnimal($animalName,$animalBirthDate,$aniChipNumber,$aniLinkPhoto,1,$aniRace);
+        }
+
 
         require('./view/addAnimal.php');
 
-    }
+      }
 
-    public function detailsAction()
-    {
+      public function detailsAction()
+      {
         require('./model/animalManager.php');
         $animalManager = new animalManager();
         $req = $animalManager->getAnimalDetail($_GET['idAnimal']);
@@ -54,9 +125,9 @@ class controllerManager
 
         require('./view/detailsAnimal.php');
 
-    }
-    public function deleteAction()
-    {
+      }
+      public function deleteAction()
+      {
         require('./model/animalManager.php');
         $animalManager = new animalManager();
         $animalManager->deleteAnimal($_GET['idAnimal']);
@@ -64,5 +135,5 @@ class controllerManager
 
         require('./view/index.php');
 
+      }
     }
-}
