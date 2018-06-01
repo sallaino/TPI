6dcd9459eb4db988553c712fea1a36c46b709b8c
Morpilou <sallaino@etml.educanet2.ch>
2018-06-01 15:41:36 +0200

    fix addAnimal select option and input's name

--- project/src/php/view/addAnimal.php
+++ project/src/php/view/addAnimal.php
@@ -10,8 +10,8 @@ $title = 'Pet La Forme - Ajouter un animal';
 
 ob_start();
 
-$nav = '';
 
+$nav = '';
 
 $typeAndRaceData = array();
 while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
@@ -31,12 +31,11 @@ while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
     <body>
 <main role="main" class="container-addAnimal">
     <div class="col-lg-8">
-        <form method="post">
+        <form method="post" action="../php/router.php?action=addAnimal">
             <div class=" row">
                 <div class="form-group col-lg-6">
                     <label for="aniType">Type d'animal</label>
-                    <select class="form-control" id="aniType">
-                        <option>-</option>
+                    <select class="form-control" name="aniType" id="aniType">
                         <?php
 
                         foreach ($typeAndRaceData as $keytad => $typeAndRaceDatum) {
@@ -59,8 +58,7 @@ while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
                 </div>
                 <div class="form-group  col-lg-6">
                     <label for="aniRace">Race de l'animal</label>
-                    <select class="form-control" id="aniRace" onChange="THEFUNCTION(this.selectedIndex);">
-                        <option>-</option>
+                    <select class="form-control" name="aniRace" id="aniRace" >
                         <?php
 
                         foreach ($typeAndRaceData as $kTARDA => $typeAndRaceDatum) {
@@ -74,7 +72,6 @@ while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
                                     <option value="<?= $keyRace ?>"><?= $race ?></option>
 
                                     <?php
-
                                 }
 
                             }
@@ -88,13 +85,13 @@ while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
             <div class="row">
                 <div class="form-group col-lg-6">
                     <label for="aniName">Nom de l'animal</label>
-                    <input type="text" class="form-control" id="aniName" aria-describedby="animal name"
+                    <input type="text" class="form-control" id="aniName" name="aniName" aria-describedby="animal name"
                            placeholder="Entrez le nom" required="required">
                     <!--          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
                 </div>
                 <div class="form-group col-lg-6">
                     <label for="aniBirthDate">Date de naissance</label>
-                    <input type="date" class="form-control" id="aniBirthDate" aria-describedby="BirthDate">
+                    <input type="date" class="form-control" name="aniBirthDate" id="aniBirthDate" aria-describedby="BirthDate">
                     <!--          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
                 </div>
             </div>
@@ -102,7 +99,7 @@ while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
             <div class="row">
                 <div class="form-group col-lg-6">
                     <label for="aniChipNumber">Numéro de puce électronmique</label>
-                    <input type="text" class="form-control" id="aniChipNumber" aria-describedby="Chip number"
+                    <input type="text" class="form-control" id="aniChipNumber" name="aniChipNumber" aria-describedby="Chip number"
                            placeholder="Entrez numéro de la puce">
                     <!--          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
                 </div>
@@ -110,7 +107,7 @@ while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
 
                 <div class="form-group col-lg-6">
                     <label for="aniWeight">Poids de l'animal</label>
-                    <input type="text" class="form-control" id="aniWeight" aria-describedby="animal weight"
+                    <input type="text" class="form-control" id="aniWeight" name="aniWeight" aria-describedby="animal weight"
                            placeholder="Entrez le poids" required="required">
                     <!--          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
                 </div>
@@ -118,18 +115,8 @@ while ($data = $typeandrace->fetch(PDO::FETCH_ASSOC)) {
 
             <div class="row">
                 <div class="form-group col-lg-6">
-                    <label for="consultation">Etat de santé de l'animal</label>
-                    <select class="form-control" id="consultation">
-                        <option>-</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-                <div class="form-group col-lg-6">
-                    <label for="exampleInputFile">Choisissez une photo pour votre animal !</label>
-                    <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                    <label for="aniLinkPhoto">Choisissez une photo pour votre animal !</label>
+                    <input name="aniLinkPhoto"type="file" class="form-control-file" id="aniLinkPhoto" aria-describedby="fileHelp">
                     <small id="fileHelp" class="form-text text-muted">La photo n'est pas obligatoire !</small>
                 </div>
             </div>
